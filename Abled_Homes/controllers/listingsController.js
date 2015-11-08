64e59695fd3bf48511d989725e87645e5444b539
siddharth bhargava <super.siddharth@gmail.com>
2015-11-08 00:58:08 -0800

    listings method working
    deleted crappy code out of listings

--- Abled_Homes/controllers/listingsController.js
+++ Abled_Homes/controllers/listingsController.js
@@ -22,43 +22,33 @@ module.exports.getListings = function(req, res) {
         	result = JSON.parse(body);
         	for (var i=0;i<result.bundle.length;i++)
         	   {
-        		listingModel.getDbListing(result.bundle[i].id, function(error, listingObject) {
-    				if (error) {
-    					logger.error('Error from database in looking up Listing. ' + error);
-    				}
-    				if (validator.isNull(listingObject)) {
-    					logger.debug('Null object received in get User controller, listingId: ' + result.bundle[i].id);
-    					var ascore=0;
-    	        	     console.log("Long lat :" + result.bundle[i].coordinates);
-    	        		 if(result.bundle[i].accessibilityFeatures!=null)
-    	        		 {
-    	        			 console.log("Accessibility Features:" + result.bundle[i].accessibilityFeatures);
-    	        			 if(result.bundle[i].accessibilityFeatures.length==1)
-    	        				 ascore=10;
-    	        			 if(result.bundle[i].accessibilityFeatures.length==2)
-    	        				 ascore=20;
-    	        			 if(result.bundle[i].accessibilityFeatures.length==3)
-    	        				 ascore=30;
-    	        		 }
-    	        		 var listing = {
-    	        				    "listingId": result.bundle[i].id,
-    	        				    "ascore": ascore
-    	        				}
-    	        		 console.log("Object to be inserted: "+ listing);
-    	        		 listingModel.dbInsertListing(listing, function(error, successmessage) {
-    	        				if (error) {
-    	        					logger.error('Error from database in Inserting Listing. ' + error);
-    	        					return res.send(500, env.errorMessages.code500);
-    	        				}
-    	        				console.log("Listing : " + successmessage);
-    	        			});
-    	        		 var appendascore = {"ascore": ascore};
-    	        		 result.bundle[i] = _.extend(result.bundle[i], appendascore);
-    				}
-    				console.log("Listing : " + listingObject.listingId +" with score " + listingObject.ascore );
-    				result.bundle[i].set('ascore',listingObject.ascore);
-    			});
-        		 
+        		console.log("Long lat :" + result.bundle[i].coordinates);
+        		var ascore=0;
+        		if(result.bundle[i].accessibilityFeatures!=null)
+           			 {
+           				 
+           				 console.log("Accessibility Features:" + result.bundle[i].accessibilityFeatures);
+           				 if(result.bundle[i].accessibilityFeatures.length==1)
+           					 ascore=10;
+           				 if(result.bundle[i].accessibilityFeatures.length==2)
+           					 ascore=20;
+           				 if(result.bundle[i].accessibilityFeatures.length==3)
+           					 ascore=30;
+           			 }
+           			 var listing = {
+            				    "listingId": result.bundle[i].id,
+            				    "ascore": ascore
+            				}
+           			 console.log("Object to be inserted: "+ listing);
+           			 listingModel.dbInsertListing(listing, function(error, successmessage) {
+            				if (error) {
+            					logger.error('Error from database in Inserting Listing. ' + error);
+            					return res.send(500, env.errorMessages.code500);
+            				}
+            				console.log("Listing : " + successmessage);
+            			});
+           			result.bundle[i].ascore=ascore;
+           			 
         	   }
         }
         else {
