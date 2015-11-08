86f1ee091c008a695c0b28f38fc5cda069bfd791
navya <navyagattupally@ymail.com>
2015-11-08 10:02:42 -0800

    API for retrieving Listing by listingId
    endpoint: /api/abledhomes/v1/listings/:listingId

--- Abled_Homes/config/init/routes.js
+++ Abled_Homes/config/init/routes.js
@@ -4,10 +4,7 @@ var listingsController = require('../../controllers/listingsController')
 module.exports = function(app, env) {
 	app.get(baseurl + '/listings', listingsController.getListings);
 	app.get(baseurl + '/listings/areaData', listingsController.getAreaData);
-//	app.post(baseurl + '/category/:category_id/product', productController.postProduct);
-//	app.get(baseurl + '/category/:category_id/product/:product_id', productController.getProduct);
-//	app.put(baseurl + '/category/:category_id/product/:product_id', productController.putProduct);
-//	app.delete(baseurl + '/category/:category_id/product/:product_id', productController.deleteProduct);
+	app.get(baseurl + '/listings/:listingId', listingsController.getOneListing);
 
 
 };
--- Abled_Homes/controllers/listingsController.js
+++ Abled_Homes/controllers/listingsController.js
@@ -1,8 +1,8 @@
 var env = require("../config/environment")
-	, validator = require("validator")
-	, _ = require("underscore")
-	, async = require('async')
-	, logger = env.logger
+  , validator = require("validator")
+  , _ = require("underscore")
+  , async = require('async')
+  , logger = env.logger
 ;
 var url=require('url');
 var retslyResults=null;
@@ -10,42 +10,42 @@ var retslyResults=null;
 
 var listingModel = require("../models/listing")
 module.exports.getListings = function(req, res) {
-	var request = require('request');
-	var queryparams=url.parse(req.url,false).query;
-		var options = {
+  var request = require('request');
+  var queryparams=url.parse(req.url,false).query;
+    var options = {
         uri : 'https://rets.io/api/v1/test/listings?access_token=6baca547742c6f96a6ff71b138424f21&limit=100&'+queryparams,
         method : 'GET'
     };
-		console.log("URI: " + options.uri);
+    console.log("URI: " + options.uri);
     var result = '';
     request(options, function (error, response, body) {
-    	if(error){
-    		logger.error('Error Getting Retsly Data ' + error);
-    	}
+      if(error){
+        logger.error('Error Getting Retsly Data ' + error);
+      }
         if (!error && response.statusCode == 200) {
-        	result = JSON.parse(body);
-        	retslyResults=result;''
-        	for (var i=0;i<retslyResults.bundle.length;i++)
+          result = JSON.parse(body);
+          retslyResults=result;''
+          for (var i=0;i<retslyResults.bundle.length;i++)
             {
-            	
+
                 var ascore=0;
                     if(retslyResults.bundle[i].accessibilityFeatures!=null)
                     {
-                       				 
-                       	console.log("Accessibility Features:" + retslyResults.bundle[i].accessibilityFeatures);
-                       	if(retslyResults.bundle[i].accessibilityFeatures.length==1)
-                       		ascore+=10;
-                       	if(retslyResults.bundle[i].accessibilityFeatures.length==2)
-                       		ascore+=20;
-                       	if(retslyResults.bundle[i].accessibilityFeatures.length==3)
-                       		ascore+=30;
+
+                        console.log("Accessibility Features:" + retslyResults.bundle[i].accessibilityFeatures);
+                        if(retslyResults.bundle[i].accessibilityFeatures.length==1)
+                          ascore+=10;
+                        if(retslyResults.bundle[i].accessibilityFeatures.length==2)
+                          ascore+=20;
+                        if(retslyResults.bundle[i].accessibilityFeatures.length==3)
+                          ascore+=30;
                      }
-                    		
+
                     if(retslyResults.bundle[i].stories<2)
-                    		ascore+=5;
+                        ascore+=5;
                     if(retslyResults.bundle[i].yearBuilt>1990)
-                    		ascore+=5;
-                       			 
+                        ascore+=5;
+
                     retslyResults.bundle[i].ascore=ascore;
                     listingModel.getNearAreaData([-122.4194160, 37.7749290], function (error, data){
                         if (error) {
@@ -57,8 +57,8 @@ module.exports.getListings = function(req, res) {
                             ascore*= 3.5;
                         } });
             }
-        	res.send(200,retslyResults);
-        	
+          res.send(200,retslyResults);
+
         }
     });
 }
@@ -80,9 +80,9 @@ var request = require("request");
 
 var options = { method: 'GET',
   url: 'https://data.sfgov.org/resource/yr47-s4hb.json',
-  headers: 
+  headers:
    {args: 'where=within_circle(location, 37.76, -122.42, 1000)',
-     'x-app-token': 'JBMRiiAfhumWQ6HkheSjGcerh' } 
+     'x-app-token': 'JBMRiiAfhumWQ6HkheSjGcerh' }
  };
 
 request(options, function (error, response, body) {
@@ -92,7 +92,25 @@ request(options, function (error, response, body) {
     console.log(body);
   }
 });
+module.exports.getOneListing = function(req, res) {
+  var request = require('request');
+  var listingId=req.params.listingId;
+  console.log("Listing ID  " + listingId);
+    var options = {
+        uri : 'https://rets.io/api/v1/test/listings/'+listingId+'?access_token=6baca547742c6f96a6ff71b138424f21&limit=1',
+        method : 'GET'
+    };
+    var result = '';
+    request(options, function (error, response, body) {
+      if(error){
+        logger.error('Error Getting Retsly Data ' + error);
+      }
+        if (!error && response.statusCode == 200) {
+          console.log("Response: " + body);
+          result = JSON.parse(body);
+          return res.send(200, result);
 
-
-
-
+        }
+});
+    console.log("URI: " + options.uri);
+}
